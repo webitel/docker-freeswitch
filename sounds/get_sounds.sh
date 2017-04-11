d020c4f9fa3ac6cbb4edb7e6088de4609dc903e1
Vitaly Kovalyshyn <v.kovalyshyn@webitel.com>
2017-04-11 17:42:45 +0300

    fixed sound permission

--- sounds/get_sounds.sh
+++ sounds/get_sounds.sh
@@ -4,7 +4,9 @@ V_MOH=1.0.52
 V_EN=1.0.51
 V_RU=1.0.51
 
-if [ !$SND_RATE ]; then
+if [ $SND_RATE ]; then 
+	echo $SND_RATE
+else
 	SND_RATE=(8000 16000 32000 48000)
 fi
 
@@ -15,6 +17,7 @@ if [ ! -d /sounds/music ] && [ "$MOH" ]; then
 		wget -O - http://files-sync.freeswitch.org/releases/sounds/freeswitch-sounds-music-${i}-$V_MOH.tar.gz | gzip -dc - | tar xf -
 	done
 	chown -R freeswitch:freeswitch /sounds/music
+	/usr/local/freeswitch/bin/fs_cli -H 172.17.0.1 -x 'reload mod_local_stream'
 fi
 
 if [ ! -d /sounds/en ] && [ "$SND_EN" ]; then
@@ -22,6 +25,7 @@ if [ ! -d /sounds/en ] && [ "$SND_EN" ]; then
 		wget -O - http://files-sync.freeswitch.org/releases/sounds/freeswitch-sounds-en-us-callie-${i}-$V_EN.tar.gz | gzip -dc - | tar xf -
 	done
 	chown -R freeswitch:freeswitch /sounds/en
+	/usr/local/freeswitch/bin/fs_cli -H 172.17.0.1 -x 'reload mod_say_en'
 fi
 
 if [ ! -d /sounds/ru ] && [ "$SND_RU" ]; then
@@ -29,6 +33,7 @@ if [ ! -d /sounds/ru ] && [ "$SND_RU" ]; then
 		wget -O - http://files-sync.freeswitch.org/releases/sounds/freeswitch-sounds-ru-RU-elena-${i}-$V_RU.tar.gz | gzip -dc - | tar xf -
 	done
 	chown -R freeswitch:freeswitch /sounds/ru
+	/usr/local/freeswitch/bin/fs_cli -H 172.17.0.1 -x 'reload mod_say_ru'
 fi
 
 exit 0
