53ad5b5b5a71e9c628dd2cdf498054077fdcd9db
Vitaly Kovalyshyn <vitaly@kovalyshyn.pp.ua>
2020-03-06 09:28:56 +0200

    update FS v1.10

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ RUN git clone https://github.com/signalwire/freeswitch.git -bv1.10 /freeswitch.g
 
 RUN cd /freeswitch.git && git config pull.rebase true \
     && rm -rf /freeswitch.git/src/mod/event_handlers/mod_amqp \
+    && git clone https://github.com/xadhoom/mod_bcg729.git /mod_bcg729 \
     && git clone https://github.com/webitel/mod_amqp.git /freeswitch.git/src/mod/event_handlers/mod_amqp \
     && git clone https://github.com/webitel/mod_amd.git /freeswitch.git/src/mod/applications/mod_amd \
     && sh bootstrap.sh -j && rm modules.conf
@@ -14,15 +15,13 @@ COPY modules.conf /freeswitch.git/modules.conf
 
 RUN cd /freeswitch.git && ./configure -C --disable-zrtp --with-soundsdir=/sounds --with-recordingsdir=/recordings --with-certsdir=/certs --with-dbdir=/db --with-scriptdir=/scripts --with-logfiledir=/logs --with-storagedir=/recordings --with-cachedir=/tmp --with-imagesdir=/images && make && make install
 
-RUN mkdir p /build
-RUN apt-get install -y curl cmake golang
-
-RUN git clone https://github.com/webitel/mod_grpc.git /mod_grpc \
-    && cd /mod_grpc && mkdir build && cd build \
-	&& cmake -DFREESWITCH_INCLUDE_DIR=/usr/local/freeswitch/include/freeswitch -DINSTALL_MOD_DIR=/usr/local/freeswitch/mod -DUSE_GRPC_VERSION=v1.19.0 -DCMAKE_BUILD_TYPE=Release .. \
-	&& make && make install
-
 RUN rm /bin/sh && ln -s /bin/bash /bin/sh
+
+RUN cd /mod_bcg729 \
+   && sed -i 's/usr\/include\/freeswitch/usr\/local\/freeswitch\/include\/freeswitch/g' Makefile \
+   && sed -i 's/usr\/lib\/freeswitch\/mod/usr\/local\/freeswitch\/mod/g' Makefile \
+   && head -n 6 Makefile \
+   && make && make install && cd / && rm -rf /mod_bcg729
 
 RUN cd / && rm -rf /usr/local/freeswitch/mod/*.la && rm -rf /usr/local/freeswitch/conf
 
@@ -76,6 +75,7 @@ COPY --from=0 /usr/local/freeswitch /usr/local/freeswitch
 COPY conf /conf
 COPY images /images
 COPY scripts /scripts
+COPY iptables-reload.sh /
 COPY docker-entrypoint.d /docker-entrypoint.d
 COPY docker-entrypoint.sh /
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,21 +1,140 @@
 #!/bin/bash
 set -e
-export PATH=/usr/local/freeswitch/bin:$PATH
+export PATH=$PATH:/usr/local/freeswitch/bin
 
 echo 'Webitel '$VERSION
+sed -i 's/WEBITEL_MAJOR/'$WEBITEL_MAJOR'/g' /conf/vars.xml
 
-if [ "$DEV" ]; then
-    export PRIVATE_IPV4="${PRIVATE_IPV4:-$(ip addr show eth0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1)}"
+if [ "$FS_CONF_FILE" ]; then
+    sed -i 's/FS_CONF_FILE/'$FS_CONF_FILE'/g' /conf/freeswitch.xml
 else
-    export PRIVATE_IPV4="${PRIVATE_IPV4:-$(ip addr show eth1 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1)}"
+    sed -i 's/FS_CONF_FILE/configuration_pgsql.xml/g' /conf/freeswitch.xml
 fi
 
-echo 'PRIVATE_IPV4 '$PRIVATE_IPV4
-
-if [ "$PRIVATE_IPV4" ]; then
-    sed -i 's/PRIVATE_IPV4/'$PRIVATE_IPV4'/g' /conf/configuration.xml
+if [ "$RTP_START_PORT" ]; then
+    sed -i 's/RTP_START_PORT/'$RTP_START_PORT'/g' /conf/configur*.xml
 else
-    sed -i '/PRIVATE_IPV4/d' /conf/configuration.xml
+    sed -i 's/RTP_START_PORT/16384/g' /conf/configur*.xml
+fi
+
+if [ "$RTP_END_PORT" ]; then
+    sed -i 's/RTP_END_PORT/'$RTP_END_PORT'/g' /conf/configur*.xml
+else
+    sed -i 's/RTP_END_PORT/32768/g' /conf/configur*.xml
+fi
+
+if [ "$MAX_SESSIONS" ]; then
+    sed -i 's/MAX_SESSIONS/'$MAX_SESSIONS'/g' /conf/configur*.xml
+else
+    sed -i 's/MAX_SESSIONS/1000/g' /conf/configur*.xml
+fi
+
+if [ "$SESSIONS_PER_SECOND" ]; then
+    sed -i 's/SESSIONS_PER_SECOND/'$SESSIONS_PER_SECOND'/g' /conf/configur*.xml
+else
+    sed -i 's/SESSIONS_PER_SECOND/30/g' /conf/configur*.xml
+fi
+
+if [ "$AMQP_HOST" ]; then
+    sed -i 's/AMQP_HOST/'$AMQP_HOST'/g' /conf/vars.xml
+else
+    sed -i 's/AMQP_HOST/172.17.0.1/g' /conf/vars.xml
+fi
+
+if [ "$PGSQL_HOST" ]; then
+    sed -i 's/PGSQL_HOST/'$PGSQL_HOST'/g' /conf/vars.xml
+else
+    sed -i 's/PGSQL_HOST/172.17.0.1/g' /conf/vars.xml
+fi
+
+if [ "$CDR_SERVER" ]; then
+    sed -i 's/CDR_SERVER/'$CDR_SERVER'/g' /conf/vars.xml
+else
+    sed -i 's/CDR_SERVER/172.17.0.1:10021/g' /conf/vars.xml
+fi
+
+if [ "$CONF_SERVER" ]; then
+    sed -i 's/CONF_SERVER/'$CONF_SERVER'/g' /conf/vars.xml
+else
+    sed -i 's/CONF_SERVER/172.17.0.1:10024/g' /conf/vars.xml
+fi
+
+if [ "$ACR_SERVER" ]; then
+    sed -i 's/ACR_SERVER/'$ACR_SERVER'/g' /conf/vars.xml
+else
+    sed -i 's/ACR_SERVER/172.17.0.1:10030/g' /conf/vars.xml
+fi
+
+if [ "$SIP_NONREG_IP" ]; then
+    sed -i 's/nonreg_ip_value/'$SIP_NONREG_IP'/g' /conf/vars.xml
+else
+    sed -i 's/nonreg_ip_value/$${local_ip_v4}/g' /conf/vars.xml
+fi
+
+if [ "$SIP_NONREG_EXT_IP" ]; then
+    sed -i 's/nonreg_ext_ip_value/'$SIP_NONREG_EXT_IP'/g' /conf/vars.xml
+else
+    sed -i 's/nonreg_ext_ip_value/auto-nat/g' /conf/vars.xml
+fi
+
+if [ "$SIP_EXTERNAL_IP" ]; then
+    sed -i 's/external_ip_value/'$SIP_EXTERNAL_IP'/g' /conf/vars.xml
+else
+    sed -i 's/external_ip_value/$${local_ip_v4}/g' /conf/vars.xml
+fi
+
+if [ "$SIP_EXTERNAL_EXT_IP" ]; then
+    sed -i 's/external_ext_ip_value/'$SIP_EXTERNAL_EXT_IP'/g' /conf/vars.xml
+else
+    sed -i 's/external_ext_ip_value/auto-nat/g' /conf/vars.xml
+fi
+
+if [ "$SIP_INTERNAL_IP" ]; then
+    sed -i 's/internal_ip_value/'$SIP_INTERNAL_IP'/g' /conf/vars.xml
+else
+    sed -i 's/internal_ip_value/$${local_ip_v4}/g' /conf/vars.xml
+fi
+
+if [ "$SIP_INTERNAL_EXT_IP" ]; then
+    sed -i 's/internal_ext_ip_value/'$SIP_INTERNAL_EXT_IP'/g' /conf/vars.xml
+else
+    sed -i 's/internal_ext_ip_value/$${local_ip_v4}/g' /conf/vars.xml
+fi
+
+if [ "$VERTO_IP" ]; then
+    sed -i 's/verto_ip_value/'$VERTO_IP'/g' /conf/vars.xml
+else
+    sed -i 's/verto_ip_value/$${local_ip_v4}/g' /conf/vars.xml
+fi
+
+if [ "$VERTO_EXT_IP" ]; then
+    sed -i 's/verto_ext_ip_value/'$VERTO_EXT_IP'/g' /conf/vars.xml
+else
+    sed -i 's/verto_ext_ip_value/$${local_ip_v4}/g' /conf/vars.xml
+fi
+
+if [ "$ES_IP" ]; then
+    sed -i 's/event_socket_ip_value/'$ES_IP'/g' /conf/vars.xml
+else
+    sed -i 's/event_socket_ip_value/172.17.0.1/g' /conf/vars.xml
+fi
+
+if [ "$LOGLEVEL" ]; then
+    sed -i 's/LOGLEVEL/'$LOGLEVEL'/g' /conf/vars.xml
+else
+    sed -i 's/LOGLEVEL/err/g' /conf/vars.xml
+fi
+
+if [ "$CODECS_GLOBAL_PREF" ]; then
+    sed -i 's/CODECS_GLOBAL_PREF/'$CODECS_GLOBAL_PREF'/g' /conf/vars.xml
+else
+    sed -i 's/CODECS_GLOBAL_PREF/OPUS,G722,PCMA,PCMU,GSM,G729,ilbc,VP8,VP9,H264,H263,H263-1998/g' /conf/vars.xml
+fi
+
+if [ "$CODECS_OUTBOUND_PREF" ]; then
+    sed -i 's/CODECS_OUTBOUND_PREF/'$CODECS_OUTBOUND_PREF'/g' /conf/vars.xml
+else
+    sed -i 's/CODECS_OUTBOUND_PREF/PCMA,PCMU,G729,GSM/g' /conf/vars.xml
 fi
 
 test -d /logs || mkdir -p /logs /certs /sounds /db /recordings /scripts/lua
@@ -24,24 +143,37 @@ chown -R freeswitch:freeswitch /usr/local/freeswitch
 chown -R freeswitch:freeswitch /{logs,tmp,db,sounds,conf,certs,scripts,recordings,images}
 
 ln -s /dev/null /dev/raw1394
-ln -s /usr/local/freeswitch/bin/fs_cli /usr/local/bin/fs_cli
+
+/iptables-reload.sh &
+/sounds/get_sounds.sh &
 
 if [ "$1" = 'freeswitch' ]; then
 
     if [ -d /docker-entrypoint.d ]; then
         for f in /docker-entrypoint.d/*.sh; do
-	    echo "$f"
-            [ -f "$f" ] && /bin/bash "$f"
+            [ -f "$f" ] && . "$f"
         done
     fi
 
     echo 'Starting FreeSWITCH '$FS_VERSION
 
-    exec freeswitch -u freeswitch -g freeswitch -c \
+    ulimit -c unlimited
+    ulimit -d unlimited
+    ulimit -f unlimited
+    ulimit -i unlimited
+    ulimit -n 999999
+    ulimit -q unlimited
+    ulimit -u unlimited
+    ulimit -v unlimited
+    ulimit -x unlimited
+    ulimit -s 240
+    ulimit -l unlimited
+
+    exec gosu freeswitch freeswitch -u freeswitch -g freeswitch -c \
         -sounds /sounds -recordings /recordings \
         -certs /certs -conf /conf -db /db \
-        -scripts /scripts -log /logs \
-        -nonat
+        -scripts /scripts -log /logs
 fi
 
 exec "$@"
+
